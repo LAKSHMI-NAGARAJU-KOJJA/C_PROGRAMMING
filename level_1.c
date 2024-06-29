dd7a5016b191e73d5128fdc7d64a7a7f24541031
LAKSHMI-NAGARAJU-KOJJA <154698197+LAKSHMI-NAGARAJU-KOJJA@users.noreply.github.com>
2024-06-29 22:51:54 +0530

    Update level_1.c

--- level_1.c
+++ level_1.c
@@ -569,6 +569,29 @@ int main(){
     }
     return 0;
 }
+// Question: Get two 2-digit numbers from user. If the sum of the numbers is less than 100, then print the sum, otherwise print the difference.
+// Example: Input: 56 78 – Output: 22
+// Input: 14 65 - Output: 79
+#include <stdio.h>
+int main(){
+    int num1,num2;
+    printf("enter two 2digits num:");
+    scanf("%d%d",&num1,&num2);
+    if(((num1<-9&&num1>-100)||(num1>9&&num1<100))&&((num2<-9&&num2>-100)||(num2>9&&num2<100))){
+        if((num1+num2)<100){
+            printf("%d",num1+num2);
+        }
+        else{
+            printf("%d",num1-num2);
+        }
+    }
+    else{
+        printf("enter a valid 2digit num!");
+    }
+    return 0;
+}
+// ======================================================================================
+
 
 
 
