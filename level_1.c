0e2054ab56638e262f71fdcd4ee7d7c1c9cdbcda
LAKSHMI-NAGARAJU-KOJJA <154698197+LAKSHMI-NAGARAJU-KOJJA@users.noreply.github.com>
2024-06-29 15:36:46 +0530

    Update level_1.c

--- level_1.c
+++ level_1.c
@@ -334,7 +334,18 @@ int main(){
     return 0;
 }
 // ======================================================================================
-
+// Question: Get a number from user and subtract 5 from that number if the number is odd, then print the result. Do not use “if”.
+// Example: Input: 695 Output 690. Input: 182 Output: 182
+#include <stdio.h>
+int main(){
+    int num,odd,res;
+    printf("enter a num:");
+    scanf("%d",&num);
+    odd = ((num%2)!=0);
+    res = num-(odd*5);
+    printf("%d",res);
+}
+// ======================================================================================
 
 
 
